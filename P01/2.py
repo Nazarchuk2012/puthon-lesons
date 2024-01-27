436d741186f1acee1a440bd66537ad692062cbaa
Nazarchuk2012 <nazarchuk.n.r@nvk9.inf.ua>
2024-01-27 11:39:56 +0200

    l1 l2

---
+++ P01/2.py
@@ -0,0 +1,14 @@
+print("start:\n")
+school name
+
+
+
+print("Мене звати ",school name,"Я живу в селі", sep="\n")
+print("Ведіть ваш вік ",age,"Я живу в селі", sep="\n")
+print("Ведіть ваше місце",Kamyanka," Я живу в селі", sep="\n")
+
+
+
+
+
+
---
+++ p02/labla.py
@@ -0,0 +1,15 @@
+print('------------------------')
+print("розрахунок лабораторій для Грю")
+print('------------------------')
+
+
+
+
+
+
+planet_area = int(input("Площа планет"))
+big_laboratory_area = int(input("Площа великий лабораторій:"))
+middle_labratory_area = int(input("Площа середніх лабораторій:"))
+small_laboratory_area = int(input("Площа малих лабораторій:"))
+
+
