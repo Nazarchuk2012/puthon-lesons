098ed97ea1936d57df4d1b2c49e11cd53a578fb5
Nazarchuk2012 <nazarchuk.n.r@nvk9.inf.ua>
2024-02-24 12:01:21 +0200

    leson komit

---
+++ p07/calculator.py
@@ -0,0 +1,34 @@
+is_run = True
+
+while is_run:
+    print('Calculator')
+    print("You can: '+', '-', '*', '/', Exit: 'x'")
+
+    action = input('Action: ')
+
+    if action == 'x':
+        is_run = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
